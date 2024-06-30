3a796a6c32361a789468fbaf31a74cbd8ca6f2f0
ZENZEPHY <adithkvxid@gmail.com>
2024-07-01 00:09:05 +0530

    Made changes to server.js, gitignore, and npm start run works, MongoDb is connected.

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+//routes
 const authRoutes = require("./routes/authRoutes");
+
+
+dotenv.config();
 const app = express();
 
+//! Database Connection
+mongoose.connect(process.env.MONGO_URI, {
+    dbName: process.env.DB_NAME,
+})
+.then(()=>{console.log("Database connected successfully");})
+.catch((err)=>{console.log("Database connection failed", err);});
+
+// ! middleware
 app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 app.use("/api",authRoutes);
 
 app.listen(3000, () => {

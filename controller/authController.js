19b64fd39b7db258a0769c8467be1b529b104e24
ZENZEPHY <adithkvxid@gmail.com>
2024-06-30 23:16:24 +0530

    .env files added and morgan added

---
+++ controller/authController.js
@@ -0,0 +1,11 @@
+const User = require("../model/authModel");
+
+//! Register User 
+
+exports.registerUser = async (req , res) => {
+    res.json("Register User");
+};
+
+//! Login User 
+
+exports.loginUser = async (req , res) => {};
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
+const authRoutes = require("./routes/authRoutes");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hi my firendo'
-    });
-});
+app.use(express.json());
+app.use("/api",authRoutes);
 
 app.listen(3000, () => {
     console.log("Server is running in some port 3000 ");
